991e98471fdfd98339683a6d1b1e77dfd6714660
Anqi-Dai <adai@bu.edu>
2019-11-26 16:10:31 -0500

    update

---
+++ scripts/lefse/run_lefse.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# This is the bash script to run lefse and generate bar plots using two input table (both tab separated)
+# This script has to be run in a conda environment that has lefse and all the dependencies installed
+# 1. pheno table that already transposed to horizontal position
+# 2. cts table that have sample names correctly sorted according to the pheno table
+
+pheno=$1
+cts=$2
+outdir=$3
+
+cat $pheno $cts > $outdir/lefse_input_table.tsv
+
+format_input.py $outdir/lefse_input_table.tsv  $outdir/lefse_input_table.in -c 1 -u 2 -o 1000000
+
+run_lefse.py $outdir/lefse_input_table.in $outdir/lefse_input_table.res
+
+plot_res.py $outdir/lefse_input_table.res  $outdir/lefse_input_table.png  --feature_font_size 4 --width 10 --dpi 300
