572d6ea84e2243db2bf6a702acff1bb168c3bc2b
Anqi-Dai <daia1@mskcc.org>
2023-02-21 15:59:27 -0500

    udpate

--- scripts/database_related/upload_data_from_query_OTU_AD.R
+++ scripts/database_related/upload_data_from_query_OTU_AD.R
@@ -30,8 +30,8 @@ library(stringr); #For string replace.
 #library(caroline); #for `dbWriteTable2`; #https://stackoverflow.com/questions/30276108/how-to-use-dbwritetable2-properly
 library("biomformat");
 library(tidyverse)
-library(vdbR)
-connect_database()
+# library(vdbR)
+# connect_database()
 get_table_from_database('samples_castori_ag')
 #library("xlsx"); #For read.xlsx function;
 
@@ -90,37 +90,37 @@ my_dbWriteTable <- function(con, table_name, d_set_to_upload_reordered){
 
 upload_data_from_query_OTU_check_and_submission <- function(table_name, d_set_to_upload ){
   #Check if d_set is in proper format to be uploaded in `table_name`.
-  
-  
+
+
   #Add uploaded_date to date_frame.
   uploaded_date = format(Sys.time(),"%m-%d-%Y");
   d_set_to_upload$uploaded_date = uploaded_date;
-  
+
   #Assign incremental key values starting on `maximum` value in current table.
-  
+
   q_key_max_cur = get_data_from_query_OTU(0.2,table_name);
   if(is.na(q_key_max_cur$max)){
     q_key_max_cur$max = 0;
   }
   d_set_to_upload$key = ( 1:length(d_set_to_upload$uploaded_date) ) + q_key_max_cur$max;
-  
+
   #Get columns for `column names`
   q_column_names = get_data_from_query_OTU(0.1,table_name);
-  
+
   #stop("Add step to compare columns, re-order data, add key and upload!");
   if( ! all(is.element(q_column_names$column_name, colnames(d_set_to_upload) )) ){
     stop( sprintf("Some fields in table %s are not defined in upload dataset", table_name));
   }
-  
+
   d_set_to_upload_reordered = d_set_to_upload[,q_column_names$column_name]
-  
+
   #Add the step to write the data into table!
   print("data getting uploaded!");
-  
+
   dbWriteTable(con, table_name, value = d_set_to_upload_reordered, append = TRUE, row.names = FALSE);
   #my_dbWriteTable(con, table_name, d_set_to_upload_reordered);
   print( sprintf("upload: done! %d rows", length(d_set_to_upload_reordered$key)));
-  
+
 }
 
 update_data_from_query_OTU_check_and_submission <- function(table_name, d_set_to_upload){
@@ -134,48 +134,48 @@ update_data_from_query_OTU_check_and_submission <- function(table_name, d_set_to
   #     4. Remove temporary table.
   #     5. Return only novel data to be uploaded;
 
-  #d_set_to_upload = d_set
+  d_set_to_upload = d_set
   #table_name = 'humann_shotgun_ko_cpm_unstratified'
   table_name <- table_name
   #d_set_to_upload <- d_set
   #Clean temp_table
   temp_table="temp_updating";
-  query_check_temp = sprintf("SELECT EXISTS (SELECT 1 FROM   information_schema.tables WHERE table_name = '%s')", 
+  query_check_temp = sprintf("SELECT EXISTS (SELECT 1 FROM   information_schema.tables WHERE table_name = '%s')",
                              temp_table);
   q_check = dbGetQuery(con,query_check_temp);
   if(q_check$exists){
-    dbSendQuery(con, sprintf("drop table %s", temp_table));  
+    dbSendQuery(con, sprintf("drop table %s", temp_table));
   }
-  
+
   #Add uploaded_date to date_frame.
   uploaded_date = format(Sys.time(),"%m-%d-%Y");
   d_set_to_upload$uploaded_date = uploaded_date;#!!!!
-  
+
   #Assign incremental key values starting on `maximum` value in current table.
   q_key_max_cur = get_data_from_query_OTU(0.2,table_name);
   if(is.na(q_key_max_cur$max)){
     q_key_max_cur$max = 0;
   }
   d_set_to_upload$key = ( 1:length(d_set_to_upload$uploaded_date) ) + q_key_max_cur$max;
-  
+
   #Get columns for `column names`
   q_column_names = get_data_from_query_OTU(0.1,table_name);
   #q_column_unique_names = get_data_from_query_OTU(0.1,sprintf("%s_unique",table_name));
-  
+
   #stop("Add step to compare columns, re-order data, add key and upload!");
   if( ! all(is.element(q_column_names$column_name, colnames(d_set_to_upload) )) ){
     stop( sprintf("Some fields in table %s are not defined in upload dataset", table_name));
   }
-  
+
   d_set_to_upload_reordered = d_set_to_upload[,q_column_names$column_name]
-  
+
   print("    creating temporary table to check for duplicates");
   query_create_temp_table = sprintf("CREATE TABLE %s AS SELECT * FROM %s WHERE 1=2",temp_table, table_name);
   dbSendQuery(con,query_create_temp_table);
-  
+
   dbWriteTable(con, temp_table, value = d_set_to_upload_reordered, append = TRUE, row.names = FALSE);
-  #my_dbWriteTable(con, table_name, d_set_to_upload_reordered);
-  
+  dbWriteTable(con, temp_table, d_set_to_upload_reordered);
+
   ###getting data that does not violate unique constrain in target table.
   query_select_unique_key = paste("select ccu.column_name, ccu.constraint_name from information_schema.table_constraints as tc",
                                   " natural join information_schema.constraint_column_usage as ccu ",
@@ -193,15 +193,15 @@ update_data_from_query_OTU_check_and_submission <- function(table_name, d_set_to
 #                                    unique_constraint_str,
 #                                    table_name);
   unique_constraint_join_str = paste(sprintf("t1.%s=t2.%s",q_unique_key$column_name,q_unique_key$column_name), collapse = " AND ")
-  
+
   query_select_novel_data = paste(sprintf("select t1.* from %s as t1 left join %s as t2", temp_table, table_name),
                                   sprintf(" on %s ", unique_constraint_join_str),
                                   sprintf("where t2.%s is NULL", q_unique_key$column_name[1]),
                                   collapse = " ");
-  
+
   d_set_unique = dbGetQuery(con,query_select_novel_data);
-  
-  print(sprintf("%d/%d novel rows to be updated!", 
+
+  print(sprintf("%d/%d novel rows to be updated!",
                 dim(d_set_unique)[1],
                 dim(d_set_to_upload_reordered)[1]) );
   if(length(d_set_unique)==0){
@@ -298,11 +298,12 @@ upload_data_from_query_OTU <- function(query_number, ...){
   }
   
   if(query_number==6){
+    library(tidyverse)
     table_name = "samples_castori_ag";
     input_data_file = '~/Desktop/tblSamples.csv'
     Samples_castori_center_file = input_data_file;
     
-    castori_downloaded_date = '2022-04-29'
+    castori_downloaded_date = '2023-02-21'
     #castori_data = read.table(Samples_castori_center_file,sep=",", quote = "", comment.char = "", header = T);
     castori_data = read_csv(Samples_castori_center_file)
     mrn_integer = as.character(castori_data$MRN);
